a505c1826f751495404a5bb1a2375965630705ef
Александр <60809442+Ubivakus@users.noreply.github.com>
2022-12-11 17:53:03 +0300

    Update Solution5.java

--- Seminar_0612/Solution5.java
+++ Seminar_0612/Solution5.java
@@ -2,16 +2,18 @@ class Solution5 {
   public boolean isPalindrome(String s) {
     StringBuilder sb = new StringBuilder();
     for (int i = 0; i < s.length(); i++) {
-      if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z')
+      if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
         sb.append(s.charAt(i));
-      if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z')
+      }
+      if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') {
         sb.append(Character.toLowerCase(s.charAt(i)));
-      if (s.charAt(i) >= '0' && s.charAt(i) <= '9')
+      }
+      if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
         sb.append(s.charAt(i));
+      }
     }
     for (int j = 0; j < sb.length(); j++) {
-      if (sb.charAt(j) != sb.charAt(sb.length() - j - 1))
-        return false;
+      if (sb.charAt(j) != sb.charAt(sb.length() - j - 1)) return false;
     }
     return true;
   }
