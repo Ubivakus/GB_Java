5a30cc9a5b4dd30be1535828a30249f1749f1fc5
Александр <60809442+Ubivakus@users.noreply.github.com>
2022-12-11 17:48:51 +0300

    Add files via upload
    Added Task 5

---
+++ Seminar_0612/Solution5.java
@@ -0,0 +1,18 @@
+class Solution5 {
+  public boolean isPalindrome(String s) {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z')
+        sb.append(s.charAt(i));
+      if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z')
+        sb.append(Character.toLowerCase(s.charAt(i)));
+      if (s.charAt(i) >= '0' && s.charAt(i) <= '9')
+        sb.append(s.charAt(i));
+    }
+    for (int j = 0; j < sb.length(); j++) {
+      if (sb.charAt(j) != sb.charAt(sb.length() - j - 1))
+        return false;
+    }
+    return true;
+  }
+}
