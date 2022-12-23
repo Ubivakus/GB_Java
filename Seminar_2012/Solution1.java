d43eefe934a7945e1b96c0abac1c1465403e2381
Александр <60809442+Ubivakus@users.noreply.github.com>
2022-12-23 20:50:56 +0300

    Update Solution1.java

--- Seminar_2012/Solution1.java
+++ Seminar_2012/Solution1.java
@@ -1,7 +1,5 @@
 import java.util.HashMap;
-//import java.util.HashSet;
 import java.util.Map;
-//import java.util.Set;
 
 public class Solution1 {
     public static int countWords(String[] words1, String[] words2) {
